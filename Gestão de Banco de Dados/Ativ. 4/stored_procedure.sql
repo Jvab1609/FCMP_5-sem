43c3d006d244bc5bceb9eb6c401f64589ece1763
João Albergaria <39803932+Jvab1609@users.noreply.github.com>
2025-05-12 11:06:48 -0300

    Trigger de cancelamento finalizado

--- Gestão de Banco de Dados/Ativ. 4/stored_procedure.sql
+++ Gestão de Banco de Dados/Ativ. 4/stored_procedure.sql
@@ -1,13 +1,19 @@
+DROP PROCEDURE  IF EXISTS `Und_DecrCap`;
+DROP PROCEDURE  IF EXISTS `Und_IncrCap`;
+DROP TRIGGER  IF EXISTS `AgendarConsulta`;
+DROP TRIGGER  IF EXISTS `CancelarConsulta`;
+
 # Diminuir a capacidade quando agendo uma consulta
 
 DELIMITER //
-CREATE PROCEDURE `Und_AtualizaCap`(`id_unid` int, `id_serv` int)
+CREATE PROCEDURE `Und_DecrCap`(`id_unid` int, `id_serv` int)
 BEGIN
 	declare cont int(4);
+    
 	SELECT capacidade INTO cont FROM unidade_has_servico WHERE unidade_id_unidade = id_unid AND servico_id_servico = id_serv;
     IF cont > 0 THEN
 		UPDATE unidade_has_servico SET capacidade = cont - 1 WHERE unidade_id_unidade = id_unid AND servico_id_servico = id_serv;
-	ELSE
+    ELSE
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'A unidade não tem capacidade no momento';
 	END IF;
@@ -16,7 +22,24 @@ END //
 CREATE TRIGGER `AgendarConsulta` BEFORE INSERT ON consulta
 FOR EACH ROW
 BEGIN
-	CALL Und_atualizaCap(new.unidade_id_unidade, new.servico_id_servico);
+	CALL Und_DecrCap(new.unidade_id_unidade, new.servico_id_servico);
+END //
+
+CREATE PROCEDURE `Und_IncrCap`(`id_unid` int, `id_serv` int)
+BEGIN
+	declare cont int(4);
+    SELECT capacidade INTO cont FROM unidade_has_servico WHERE unidade_id_unidade = id_unid AND servico_id_servico = id_serv;
+	UPDATE unidade_has_servico SET capacidade = cont + 1 WHERE unidade_id_unidade = id_unid AND servico_id_servico = id_serv;
+END //
+
+CREATE TRIGGER `CancelarConsulta` AFTER UPDATE ON consulta
+FOR EACH ROW
+BEGIN
+	IF new.data_cancelamento IS NOT null THEN
+		CALL Und_IncrCap(new.unidade_id_unidade, new.servico_id_servico);
+	ELSE
+		CALL Und_DecrCap(new.unidade_id_unidade, new.servico_id_servico);
+	END IF;
 END //
 
 DELIMITER ;
--- Gestão de Banco de Dados/Ativ. 4/teste_procedure.sql
+++ Gestão de Banco de Dados/Ativ. 4/teste_procedure.sql
@@ -1,2 +1,10 @@
+DELETE FROM consulta WHERE id_consulta = 100001;
+
+START TRANSACTION;
 INSERT INTO `consulta` VALUES 
-(100001, '2025-05-03 15:13:47', '2025-06-10 10:45:00', null, null, null, 'obs_1', null, 2, 56841, 3, 56841, 2)
+(100001, '2025-05-03 15:13:47', '2025-06-10 10:45:00', null, null, null, 'obs_1', null, 2, 56841, 3, 56841, 2);
+COMMIT;
+
+START TRANSACTION;
+UPDATE `consulta` SET data_cancelamento = '2025-05-12' WHERE id_consulta = '100001';
+COMMIT;
