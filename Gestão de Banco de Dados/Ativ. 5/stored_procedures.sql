591132d8d0324b09b3ca762ea3308c8aeafad6da
João Albergaria <jvalbergariabar@gmail.com>
2025-05-22 23:41:41 -0300

    Count_Profs + Modelo de IA

--- Gestão de Banco de Dados/Ativ. 5/stored_procedures.sql
+++ Gestão de Banco de Dados/Ativ. 5/stored_procedures.sql
@@ -1,6 +1,6 @@
 USE escola;
 DROP PROCEDURE  IF EXISTS `Count_Alunos`;
-
+DROP PROCEDURE  IF EXISTS `Count_Profs`;
 
 # Diminuir a capacidade quando agendo uma consulta
 
@@ -9,5 +9,12 @@ CREATE PROCEDURE `Count_Alunos`(IN `id_curso` int)
 BEGIN
 	SELECT COUNT(*) FROM aluno_has_curso WHERE curso_id_curso = id_curso;
 END //
+
+DELIMITER //
+CREATE PROCEDURE `Count_Profs`(IN `id_curso` int)
+BEGIN
+	SELECT COUNT(*) FROM professor WHERE curso_id_curso = id_curso;
+END //
+
 
 DELIMITER ;
--- Gestão de Banco de Dados/Ativ. 5/teste_sp.sql
+++ Gestão de Banco de Dados/Ativ. 5/teste_sp.sql
@@ -1,2 +1,3 @@
 USE escola;
 CALL Count_Alunos(11);
+CALL Count_Profs(11);
