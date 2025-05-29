073bdea72e2102636bdf432a9799cb6280ac5eef
João Albergaria <jvalbergariabar@gmail.com>
2025-05-29 00:34:17 -0300

    Insert de pedidos automatizado (falta testar)

--- Gestão de Banco de Dados/Prova B3/inserts/insert_pedido.py
+++ Gestão de Banco de Dados/Prova B3/inserts/insert_pedido.py
@@ -1,6 +1,7 @@
 import random
 from faker import Faker
 import mysql.connector
+from datetime import timedelta
 
 fake = Faker('pt_BR') 
 
@@ -10,7 +11,7 @@ cursor = conn.cursor()
 cursor.execute("START TRANSACTION")
 conn.commit()
 
-for i in range(500):
+for i in range(2000):
 
     query = ("SELECT COUNT(*) FROM entregador;")
     cursor.execute(query)
@@ -31,62 +32,65 @@ for i in range(500):
     restaurante_id = random.randint(1, count_restaurante)
     cliente_id = random.randint(1, count_cliente)
 
-
-    nome = fake.name()
-    cpf = fake.cpf()
-
-    params = [nome, cpf]  
-
-    cursor.execute("INSERT INTO cliente VALUES (null, %s, %s, 0);", params)
-    conn.commit()
-    
-    query = ("SELECT last_insert_id();")
+    query = ("SELECT endereco_cep, endereco_num_end FROM cliente_has_endereco WHERE cliente_id_cliente = %s", [cliente_id])
     cursor.execute(query)
     rows_consulta = cursor.fetchall()
-    cliente_id = rows_consulta[0][0]
+    cep = rows_consulta[0][0]
+    numero = rows_consulta[0][1]
 
-    telefone = fake.phone_number()
-    params = [telefone, cliente_id]
+    query = ("SELECT COUNT(*) FROM prato WHERE restaurante_id_restaurante = %s;", [restaurante_id])
+    cursor.execute(query)
+    rows_consulta = cursor.fetchall()
+    count_pratos = rows_consulta[0][0]
 
-    cursor.execute("INSERT INTO telefone VALUES (%s, null, null, %s)", params)
-    conn.commit()
+    query = ("SELECT id_prato FROM prato WHERE restaurante_id_restaurante = %s;", [restaurante_id])
+    cursor.execute(query)
+    rows_consulta = cursor.fetchall()
+    prato_1 = rows_consulta[0][0]
 
-    username = f"user_entr_{nome.lower().split(' ')[0]}{i}"
-    email = f"{nome.lower().replace(' ', '.')}{i}@gmail.com"
-    senha = f"senha_{nome.lower()}"
-    params = [username, email, senha, cliente_id]
+    pratos = []
+    for j in range (random.randint(1, 4)):
+        novo_prato = prato_1 + random.randint(0, count_pratos - 1)
+        if (novo_prato not in pratos):
+            params = [i, novo_prato]
+            cursor.execute("INSERT INTO pedido_has_prato VALUES (%s, %s);", params)
+            conn.commit()
+            pratos.append(novo_prato)
 
-    cursor.execute("INSERT INTO login VALUES(null, %s, %s, sha1(%s), null, null, %s)", params)
-    conn.commit()
 
-    cep = fake.postcode()
-    numero = random.randint(1, 2000)
-    rua = fake.street_name()
-    bairro = fake.bairro()
-    cidade = fake.city()
-    estado = fake.estado_sigla()
+    recebido = fake.date_time()
+    inicio_preparo = recebido + timedelta(minutes=random.randint(1, 15))
+    
+    if (random.randint(1, 8) == 8):
+        cancelamento = inicio_preparo + timedelta(minutes=random.randint(31, 40))
 
-    params = [cep, numero, rua, bairro, cidade, estado]
-    cursor.execute("INSERT INTO endereco VALUES (%s, %s, %s, %s, %s, %s, null, null);", params)
-    conn.commit()
-
-    params = [cliente_id, cep, numero]
-    cursor.execute("INSERT INTO cliente_has_endereco VALUES ('CASA', %s, %s, %s);", params)
-    conn.commit()
-
-    if (random.randint(1, 4) == 4):
-        cep = fake.postcode()
-        numero = random.randint(1, 2000)
-        rua = fake.street_name()
-        bairro = fake.bairro()
-
-        params = [cep, numero, rua, bairro, cidade, estado]
-        cursor.execute("INSERT INTO endereco VALUES (%s, %s, %s, %s, %s, %s, null, null);", params)
+        params = [recebido, inicio_preparo, cancelamento, restaurante_id, entregador_id, cliente_id, cep, numero]  
+        cursor.execute("INSERT INTO pedido VALUES (null, 'CANCELADO', %s, %s, null, null, %s, %s, %s, %s, %s, %s);", params)
         conn.commit()
+        continue
 
-        params = [cliente_id, cep, numero]
-        cursor.execute("INSERT INTO cliente_has_endereco VALUES ('TRABALHO', %s, %s, %s);", params)
-        conn.commit()
+    saida = inicio_preparo + timedelta(minutes=random.randint(10, 25))
+    entrega = saida + timedelta(minutes=random.randint(8, 23))
+    params = [recebido, inicio_preparo, saida, entrega, restaurante_id, entregador_id, cliente_id, cep, numero]  
+    cursor.execute("INSERT INTO pedido VALUES (null, 'ENTREGUE', %s, %s, %s, %s, null, %s, %s, %s, %s, %s);", params)
+    conn.commit()
+
+    pagamento = entrega - timedelta(minutes=random.randint(1, 3))
+    formas = ['Débito', 'Crédito', 'Pix', 'VR']
+    forma = formas[random.randint(0, 3)]
+    
+    valor = 0
+    for j in range(0, len(pratos)):
+        query = ("SELECT preco FROM prato WHERE id_prato = %s;", [pratos[j]])
+        cursor.execute(query)
+        rows_consulta = cursor.fetchall()
+        preco = rows_consulta[0][0]
+        valor += preco
+
+    params = [pagamento, forma, valor, i]  
+    cursor.execute("INSERT INTO pagamento VALUES (null, %s, %s, %s, %s);", params)
+    conn.commit()
+
     print(i)
 
 cursor.execute("COMMIT")
