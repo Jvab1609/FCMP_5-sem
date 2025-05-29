8be5f8fef361831614efced4cbf50cbe6c00bf92
João Albergaria <jvalbergariabar@gmail.com>
2025-05-29 17:51:43 -0300

    Mostra os restaurantes e entregadores que fizeram mais pedidos

--- Gestão de Banco de Dados/Prova B3/teste_sp.sql
+++ Gestão de Banco de Dados/Prova B3/teste_sp.sql
@@ -5,6 +5,14 @@ ADD classificacao VARCHAR(7);
 
 CALL ClassifRest();
 
-#ALTER TABLE entregador
-#ADD classificacao VARCHAR(7);
+ALTER TABLE entregador
+ADD classificacao VARCHAR(7);
+CALL ClassifEntr();
 
+CALL TopRestPedidos(1, 3);
+CALL TopRestPedidos(0, 3);
+
+CALL TopEntrPedidos(1, 3);
+CALL TopEntrPedidos(0, 3);
+
+SELECT count(*) FROM information_schema.columns WHERE table_name ='entregador';
--- Gestão de Banco de Dados/Prova B3/triggers_stored_proc.sql
+++ Gestão de Banco de Dados/Prova B3/triggers_stored_proc.sql
@@ -2,7 +2,8 @@ USE delivery;
 
 DROP PROCEDURE  IF EXISTS `ClassifRest`;
 DROP PROCEDURE  IF EXISTS `ClassifEntr`;
-
+DROP PROCEDURE  IF EXISTS `TopRestPedidos`;
+DROP PROCEDURE  IF EXISTS `TopEntrPedidos`;
 DROP TRIGGER  IF EXISTS `CalcularMedia`;
 
 
@@ -19,16 +20,18 @@ BEGIN
     
     REPEAT
 		FETCH nota_cursor INTO nota_i;
-        IF nota_i > 4.5 THEN
+        IF nota_i >= 4.5 THEN
 			UPDATE restaurante SET classificacao = 'OTIMO' WHERE nota_med_restaurante = nota_i ;
-		ELSEIF nota_i >= 4 AND nota_i <= 4.5 THEN
+		ELSEIF nota_i >= 4 AND nota_i < 4.5 THEN
 			UPDATE restaurante SET classificacao = 'BOM' WHERE nota_med_restaurante = nota_i;
-		ELSEIF nota_i >= 3 AND nota_i <= 4 THEN
+		ELSEIF nota_i >= 3 AND nota_i < 4 THEN
 			UPDATE restaurante SET classificacao = 'MEDIANO' WHERE nota_med_restaurante = nota_i;
-        ELSEIF nota_i >= 2 AND nota_i <= 3 THEN
+        ELSEIF nota_i >= 2 AND nota_i < 3 THEN
 			UPDATE restaurante SET classificacao = 'RUIM' WHERE nota_med_restaurante = nota_i;    
-		ELSE
+		ELSEIF nota_i >= 0 AND nota_i < 2 THEN
 			UPDATE restaurante SET classificacao = 'PESSIMO' WHERE nota_med_restaurante = nota_i;
+		ELSE
+			UPDATE restaurante SET classificacao = 'NA' WHERE nota_med_restaurante = nota_i;
 		END IF;
     UNTIL pronto
     END REPEAT;
@@ -50,22 +53,50 @@ BEGIN
     
     REPEAT
 		FETCH nota_cursor INTO nota_i;
-        IF nota_i > 4.5 THEN
+        IF nota_i >= 4.5 THEN
 			UPDATE entregador SET classificacao = 'OTIMO' WHERE nota_media_entregador = nota_i ;
-		ELSEIF nota_i >= 4 AND nota_i <= 4.5 THEN
+		ELSEIF nota_i >= 4 AND nota_i < 4.5 THEN
 			UPDATE entregador SET classificacao = 'BOM' WHERE nota_media_entregador = nota_i;
-		ELSEIF nota_i >= 3 AND nota_i <= 4 THEN
+		ELSEIF nota_i >= 3 AND nota_i < 4 THEN
 			UPDATE entregador SET classificacao = 'MEDIANO' WHERE nota_media_entregador = nota_i;
-        ELSEIF nota_i >= 2 AND nota_i <= 3 THEN
-			UPDATE entregador SET classificacao = 'RUIM' WHERE nota_media_entregador = nota_i;    
-		ELSE
+        ELSEIF nota_i >= 2 AND nota_i < 3 THEN
+			UPDATE entregador SET classificacao = 'RUIM' WHERE nota_media_entregador = nota_i; 
+		ELSEIF nota_i >= 0 AND nota_i < 2 THEN
 			UPDATE entregador SET classificacao = 'PESSIMO' WHERE nota_media_entregador = nota_i;
+		ELSE
+			UPDATE entregador SET classificacao = 'NA' WHERE nota_media_entregador = nota_i;
 		END IF;
     UNTIL pronto
     END REPEAT;
     
     CLOSE nota_cursor;
 END //
+
+CREATE PROCEDURE `TopRestPedidos` (IN modo int, IN limite int)
+BEGIN
+	IF modo = 1 THEN
+		SELECT restaurante.id_restaurante, restaurante.nome_rest, COUNT(pedido.id_pedido) FROM restaurante
+		INNER JOIN pedido ON pedido.restaurante_id_restaurante = restaurante.id_restaurante
+		GROUP BY restaurante.id_restaurante ORDER BY COUNT(*) DESC LIMIT limite;
+    ELSE
+		SELECT restaurante.id_restaurante, restaurante.nome_rest, COUNT(pedido.id_pedido) FROM restaurante
+		INNER JOIN pedido ON pedido.restaurante_id_restaurante = restaurante.id_restaurante
+		GROUP BY restaurante.id_restaurante ORDER BY COUNT(*) ASC LIMIT limite;
+	END IF;
+END // 
+
+CREATE PROCEDURE `TopEntrPedidos` (IN modo int, IN limite int)
+BEGIN
+	IF modo = 1 THEN
+		SELECT entregador.id_entregador, entregador.nome_entregador, COUNT(pedido.id_pedido) FROM entregador
+		INNER JOIN pedido ON pedido.entregador_id_entregador = entregador.id_entregador
+		GROUP BY entregador.id_entregador ORDER BY COUNT(*) DESC LIMIT limite;
+    ELSE
+		SELECT entregador.id_entregador, entregador.nome_entregador, COUNT(pedido.id_pedido) FROM entregador
+		INNER JOIN pedido ON pedido.entregador_id_entregador = entregador.id_entregador
+		GROUP BY entregador.id_entregador ORDER BY COUNT(*) ASC LIMIT limite;
+	END IF;
+END // 
 
 
 DELIMITER //
@@ -76,6 +107,9 @@ BEGIN
     declare avgEntr float(4);
     declare idRest int;
     declare idEntr int;
+    
+    declare restColCount int;
+    declare entrColCount int;
     
 	# Pegando o ID do restaurante
     SELECT pedido.restaurante_id_restaurante INTO idRest FROM pedido
@@ -98,5 +132,44 @@ BEGIN
     
 	UPDATE restaurante SET nota_med_restaurante = avgRest WHERE id_restaurante = idRest;
     UPDATE entregador SET nota_media_entregador = avgEntr WHERE id_entregador = idEntr;
-	#CALL Und_DecrCap(new.unidade_id_unidade, new.servico_id_servico);
+    
+    
+	SELECT count(*) INTO entrColCount FROM information_schema.columns WHERE table_name ='entregador';
+    
+    IF entrColCount >= 7 THEN
+		IF avgEntr >= 4.5 THEN
+			UPDATE entregador SET classificacao = 'OTIMO' WHERE nota_media_entregador = avgEntr ;
+		ELSEIF avgEntr >= 4 AND avgEntr < 4.5 THEN
+			UPDATE entregador SET classificacao = 'BOM' WHERE nota_media_entregador = avgEntr;
+		ELSEIF avgEntr >= 3 AND avgEntr < 4 THEN
+			UPDATE entregador SET classificacao = 'MEDIANO' WHERE nota_media_entregador = avgEntr;
+        ELSEIF avgEntr >= 2 AND avgEntr < 3 THEN
+			UPDATE entregador SET classificacao = 'RUIM' WHERE nota_media_entregador = avgEntr; 
+		ELSEIF avgEntr >= 0 AND avgEntr < 2 THEN
+			UPDATE entregador SET classificacao = 'PESSIMO' WHERE nota_media_entregador = avgEntr;
+		ELSE
+			UPDATE entregador SET classificacao = 'NA' WHERE nota_media_entregador = avgEntr;
+		END IF;
+	END IF;
+    
+    SELECT count(*) INTO restColCount FROM information_schema.columns WHERE table_name ='restaurante';
+    IF restColCount >= 8 THEN
+		IF avgRest >= 4.5 THEN
+			UPDATE restaurante SET classificacao = 'OTIMO' WHERE nota_med_restaurante = avgRest;
+		ELSEIF avgRest >= 4 AND avgRest < 4.5 THEN
+			UPDATE restaurante SET classificacao = 'BOM' WHERE nota_med_restaurante = avgRest;
+		ELSEIF avgRest >= 3 AND avgRest < 4 THEN
+			UPDATE restaurante SET classificacao = 'MEDIANO' WHERE nota_med_restaurante = avgRest;
+        ELSEIF avgRest >= 2 AND avgRest < 3 THEN
+			UPDATE restaurante SET classificacao = 'RUIM' WHERE nota_med_restaurante = avgRest;    
+		ELSEIF avgRest >= 0 AND avgRest < 2 THEN
+			UPDATE restaurante SET classificacao = 'PESSIMO' WHERE nota_med_restaurante = avgRest;
+		ELSE
+			UPDATE restaurante SET classificacao = 'NA' WHERE nota_med_restaurante = avgRest;
+		END IF;
+	END IF;
+    
 END //
+
+
+
