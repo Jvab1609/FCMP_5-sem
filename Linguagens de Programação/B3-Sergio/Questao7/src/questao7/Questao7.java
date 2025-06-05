e7603fcd4787ea8072788346a2b593aee59ed63d
João Albergaria <jvalbergariabar@gmail.com>
2025-06-05 00:08:23 -0300

    Deletado o projeto não usado

--- Linguagens de Programação/B3-Sergio/Questao7/src/questao7/Questao7.java
+++ Linguagens de Programação/B3-Sergio/Questao7/src/questao7/Questao7.java
@@ -13,30 +13,8 @@ public class Questao7 {
     /**
      * @param args the command line arguments
      */
-    
-    
-    public static int contador = 0;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         // TODO code application logic here
-        
-        Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
-                contador++;
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
-                contador++;
-            }
-        });
-
-        t1.start();
-        t1.join();
-        t2.start();
-        t2.join();
-        
-        
-        System.out.println("Valor final do contador: " + contador);
     }
     
 }
